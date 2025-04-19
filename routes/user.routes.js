5a96ef81cfadebc78de13d66a75c04d889d30c31
Sumit Raj <sumit.does.code@gmail.com>
2025-04-19 16:19:10 +0530

    changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getOwnProfile, getUserProfileById, editOwnProfile, usersToFollow, followOrUnfollowUser } from "../controllers/user.controller.js";
+import { getUserProfileById, editOwnProfile, usersToFollow, followOrUnfollowUser } from "../controllers/user.controller.js";
 import { requireAuth } from "@clerk/express";
 import upload from "../middlewares/multer.middleware.js";
 
@@ -7,12 +7,6 @@ const router = express.Router();
 
 // here id is mongodb id of user
 // prefix is /api/v1/user
-const customRequireAuth = (req, res, next) => {
-    console.log("✅ requireAuth hit!");
-    return requireAuth()(req, res, next);
-};
-
-router.get("/", customRequireAuth, getOwnProfile); // get own profile
 router.get("/:id", getUserProfileById); // get user profile by id
 router.patch("/", requireAuth(), upload.single("profileImage"), editOwnProfile); // edit user profile(self)
 router.get("/usersToFollow", requireAuth(), usersToFollow); // get users to follow
