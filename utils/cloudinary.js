72e26a6fafdb0b6d408e22fa72f35824b04b5202
Sumit Raj <sumit.does.code@gmail.com>
2025-04-19 17:21:45 +0530

    minor

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,5 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import { config } from "dotenv";
+config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
