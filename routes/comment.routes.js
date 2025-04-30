f05b7ea388fdc48483d9190458cd8722c121b89d
Sumit Raj <sumit.does.code@gmail.com>
2025-04-30 13:52:39 +0530

    lkajdf

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -4,8 +4,8 @@ import { requireAuth } from "@clerk/express";
 
 const router = express.Router();
 
-router.get("post/:postId", requireAuth(), getPostComments); // Get all comments for a post
-router.post("post/:postId", requireAuth(), createComment); // Add a new comment
+router.get("/post/:postId", requireAuth(), getPostComments); // Get all comments for a post
+router.post("/post/:postId", requireAuth(), createComment); // Add a new comment
 router.delete("/:commentId", requireAuth(), deleteComment); // Delete a comment
 
 export default router;
