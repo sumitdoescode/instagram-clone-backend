8c79c2b1c1d9299def2ab69045370444390e4ddb
Sumit Raj <sumit.does.code@gmail.com>
2025-06-08 04:29:06 +0530

    another

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -90,8 +90,18 @@ export const getMessages = asyncHandler(async (req, res) => {
     }
 
     // Step 2: Fetch messages by conversationId
-    const messages = await Message.find({ conversationId: conversation._id }).sort({ createdAt: -1 });
-
+    const senderMessages = await Message.find({
+        conversationId: conversation._id,
+        senderId: currentUser._id,
+    }).sort({ createdAt: -1 });
+    const receiverMessages = await Message.find({
+        conversationId: conversation._id,
+        receiverId: currentUser._id,
+    }).sort({ createdAt: -1 });
+    const messages = {
+        senderMessages: senderMessages,
+        receiverMessages: receiverMessages,
+    };
     res.status(200).json({
         success: true,
         message: "Messages fetched successfully",
